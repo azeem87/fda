fcd8e9e0ba11c3b196ed94e375539780435c6dab
Mohamed Azeem <azeem.fx@gmail.com>
2020-07-24 21:16:55 +0400

    updated settings for eslint

--- backend/events-handlers/src/index.js
+++ backend/events-handlers/src/index.js
@@ -4,7 +4,7 @@ import morganBody from 'morgan-body';
 
 // Application Route Handler Import //
 // when an order is created it validates order and its eligibility.
-import creeateOrderEventHandler from './create-order';
+import createOrderEventHandler from './create-order';
 
 const express = require('express');
 const app = express();
@@ -16,20 +16,23 @@ const context = process.env.CONTEXT || '/api/v1';
 //add middleware's
 app.use(cors());
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
-morganBody(app,{logAllReqHeader:true});
+app.use(bodyParser.urlencoded({ extended: true }));
+morganBody(app, { logAllReqHeader: true });
 
 // ROUTE START //
 app.get(`${context}/`, (req, res) => res.send('the organic fresh app API!'));
-app.post(`${context}/create-order-event`, asyncHandler(creeateOrderEventHandler));
+app.post(
+  `${context}/create-order-event`,
+  asyncHandler(createOrderEventHandler)
+);
 app.use(errorMiddleware);
 // If no matches found, return 404
-app.use((req, res) => {throw new NotFoundError()});
+app.use((req, res) => {
+  throw new NotFoundError();
+});
 // ROUTES END  //
 
 //start app
 const port = process.env.PORT || 3000;
 
-app.listen(port, () =>
-    console.log(`App is listening on port ${port}.`)
-);
+app.listen(port, () => console.log(`App is listening on port ${port}.`));
--- backend/events-handlers/src/knex-pg.js
+++ backend/events-handlers/src/knex-pg.js
@@ -1,15 +1,12 @@
 // Initialize Knex with PG
 
-console.log( 'PG CONNECTION STRING -> '+ process.env.PG_CONNECTION_STRING);
+console.log('PG CONNECTION STRING -> ' + process.env.PG_CONNECTION_STRING);
 
 const knex = require('knex')({
-    client: 'pg',
-    connection: process.env.PG_CONNECTION_STRING,
-    searchPath: ['public'],
-    pool: { min: 0, max: 7 },
+  client: 'pg',
+  connection: process.env.PG_CONNECTION_STRING,
+  searchPath: ['public'],
+  pool: { min: 0, max: 7 }
 });
 
 export default knex;
-
-
-
